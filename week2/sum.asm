9fb38ae12c5c9bd379bf8e481cd9e75cff2d0611
aaaditt <aaditchandra2212@gmail.com>
2026-09-01 20:15:52 +0400

    Add week 2 assembly programs (sum.asm and first_n.asm)

---
+++ week2/sum.asm
@@ -0,0 +1,36 @@
+        .data
+msg1: .asciiz "Enter the first number: "
+msg2: .asciiz "Enter the second number: "
+res: .asciiz "the result is: "
+
+        .text
+main:   li $v0,4
+        la $a0,msg1
+        syscall
+
+        li,$v0,5
+        syscall
+
+        move $t0,$v0
+
+        li $v0,4
+        la $a0,msg2
+        syscall
+
+        li,$v0,5
+        syscall
+
+        move $t1,$v0
+
+        add $t2,$t0,$t1
+
+        li,$v0,4
+        la $a0,res
+        syscall
+
+        li,$v0,1
+        move $a0,$t2
+        syscall
+
+        li,$v0,10
+        syscall

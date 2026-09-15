ebd5a07bfa6bb52ed308e220e5eb7f0f31c16df9
aaaditt <aaditchandra2212@gmail.com>
2026-09-15 11:25:21 +0400

    new week

---
+++ week4/priority.asm
@@ -0,0 +1,55 @@
+    .data
+inp1: .asciiz "Enter the first integer: "
+inp2: .asciiz "Enter the second integer: "
+inp3: .asciiz "Enter the third integer: "
+inp4: .asciiz "ENter the fourth integer: "
+inp5: .asciiz "Enter the fifth integer: "
+res: .asciiz "The ans is: "
+
+    .text
+
+main:   li $v0,4
+        la $a0,inp1
+        syscall
+
+        li $v0,5
+        syscall
+        move $t0,$v0
+
+        li $v0,4
+        la $a0,inp2
+        syscall
+
+        li $v0,5
+        syscall
+        move $t1,$v0
+
+        li $v0,4
+        la $a0,inp3
+        syscall
+
+        li $v0,5
+        syscall
+        move $t2,$v0
+
+        li $v0,4
+        la $a0,inp4
+        syscall
+
+        li $v0,5
+        syscall
+        move $t3,$v0
+
+        li $v0,4
+        la $a0,inp5
+        syscall
+
+        li $v0,5
+        syscall
+        move $t4,$v0
+        
+        mult $t0,$t1
+        mflo $t5
+        mfhi $t6
+        div 
+

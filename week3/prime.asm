f1871baa17e09fd29b28a8cb7d147e0e0f280ed2
aaaditt <aaditchandra2212@gmail.com>
2026-09-15 12:24:49 +0400

    prime number code done

--- week3/prime.asm
+++ week3/prime.asm
@@ -14,20 +14,30 @@ main:
     move $t0,$v0
 
     li $t1,2
+    blt $t0,$t1,not_prime
+
+loop:
+    mul $t3,$t1,$t1
+    bgt $t3,$t0,prime
+
     div $t0,$t1
     mfhi $t2
+    beq $t2,$zero,not_prime
 
-    bne $t2,$zero,prime
+    addi $t1,$t1,1
+    j loop
 
-    prime:
-        li $v0,4
-        la $a0,res1
-        syscall
+prime:
+    li $v0,4
+    la $a0,res1
+    syscall
+    j exit
 
-    not_prime:
-        li $v0,4
-        la $a0,res2
-        syscall
+not_prime:
+    li $v0,4
+    la $a0,res2
+    syscall
 
+exit:
     li $v0,10
     syscall
